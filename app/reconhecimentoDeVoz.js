59d3f116289bde2613da31df36c01eed5b16120a
mtserik <erik.stos.mts@gmail.com>
2022-11-08 11:45:02 -0300

    Criando as validacoes e mensagens de retorno com erros

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,6 +1,6 @@
 const elementoChute = document.getElementById('chute');
 
-window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+//window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
@@ -11,6 +11,7 @@ recognition.addEventListener("result", onSpeak);
 function onSpeak(e) {
     chute = e.results[0][0].transcript;
     exibeChuteNaTela(chute);
+    verificaSeOChutePossuiUmValorValido(chute);
 }
 
 function exibeChuteNaTela(chute) {
---
+++ app/validacao.js
@@ -0,0 +1,19 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        console.log("Valor inválido");
+    }
+
+    if (numeroForMaiorOuMenor(numero)) {
+        console.log(`Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`);
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenor(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
