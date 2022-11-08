3f7feb430c0ead03c3afc2054096bd3758b4cfe7
mtserik <erik.stos.mts@gmail.com>
2022-11-08 13:10:10 -0300

    Criando evento para que possa continuar chutando e adicionando botao de jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -3,19 +3,33 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += "<div>Valor inválido</div>";
+        return;
     }
 
     if (numeroForMaiorOuMenor(numero)) {
         elementoChute.innerHTML +=`
         <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
         `;
+        return;
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-        `
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `;
+        recognition.stop();
+
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `;
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `;
     }
 }
 
@@ -26,3 +40,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenor(numero) {
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener("click", e => {
+    if (e.target.id == "jogar-novamente") {
+        window.location.reload();
+    }
+})
